84267526b47895382a33dd34799e882285291d8d
Bohdon Sayre <bohdon@gmail.com>
2023-11-09 11:20:21 -0500

    initial commit

---
+++ Plugins/MGFX/Source/MGFX/Private/MGFXModule.cpp
@@ -0,0 +1,17 @@
+﻿// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MGFXModule.h"
+
+#define LOCTEXT_NAMESPACE "FMGFXModule"
+
+void FMGFXModule::StartupModule()
+{
+}
+
+void FMGFXModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FMGFXModule, MGFX)
