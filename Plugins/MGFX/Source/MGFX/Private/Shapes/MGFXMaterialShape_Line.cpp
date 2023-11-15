41f217dbf65e04cd3f98be45eba2486d1a2c4f91
Bohdon Sayre <bohdon@gmail.com>
2023-11-15 12:22:24 -0500

    add line shape

---
+++ Plugins/MGFX/Source/MGFX/Private/Shapes/MGFXMaterialShape_Line.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+
+#include "Shapes/MGFXMaterialShape_Line.h"
+
+#include "Shapes/MGFXMaterialShapeVisual.h"
+
+
+UMGFXMaterialShape_Line::UMGFXMaterialShape_Line()
+{
+#if WITH_EDITORONLY_DATA
+	MaterialFunction = TSoftObjectPtr<UMaterialFunctionInterface>(FString("/MGFX/MaterialFunctions/MF_MGFX_Shape_Line.MF_MGFX_Shape_Line"));
+#endif
+
+	Visuals.Add(CreateDefaultSubobject<UMGFXMaterialShapeStroke>(TEXT("DefaultStroke")));
+}
+
+#if WITH_EDITORONLY_DATA
+TArray<FMGFXMaterialShapeInput> UMGFXMaterialShape_Line::GetInputs() const
+{
+	TArray<FMGFXMaterialShapeInput> Result;
+
+	Result.Emplace(FMGFXMaterialShapeInput::Vector2(TEXT("PointA"), PointA));
+	Result.Emplace(FMGFXMaterialShapeInput::Vector2(TEXT("PointB"), PointB));
+
+	return Result;
+}
+#endif
---
+++ Plugins/MGFX/Source/MGFX/Public/Shapes/MGFXMaterialShape_Line.h
@@ -0,0 +1,32 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MGFXMaterialShape.h"
+#include "MGFXMaterialShape_Line.generated.h"
+
+
+/**
+ * A line segment with two points.
+ */
+UCLASS(DisplayName = "Line")
+class MGFX_API UMGFXMaterialShape_Line : public UMGFXMaterialShape
+{
+	GENERATED_BODY()
+
+public:
+	UMGFXMaterialShape_Line();
+
+	UPROPERTY(EditAnywhere, Category = "Line")
+	FVector2f PointA = FVector2f(0.f, 0.f);
+
+	UPROPERTY(EditAnywhere, Category = "Line")
+	FVector2f PointB = FVector2f(100.f, 0.f);
+
+	virtual FString GetShapeName() const override { return "Line"; }
+
+#if WITH_EDITORONLY_DATA
+	virtual TArray<FMGFXMaterialShapeInput> GetInputs() const override;
+#endif
+};
