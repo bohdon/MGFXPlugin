72d87fa1bcc27b3b899b22b166f344f0f9aaa5dd
Bohdon Sayre <bohdon@gmail.com>
2023-11-15 02:45:00 -0500

    add cross shape

---
+++ Plugins/MGFX/Source/MGFX/Private/Shapes/MGFXMaterialShape_Cross.cpp
@@ -0,0 +1,27 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+
+#include "Shapes/MGFXMaterialShape_Cross.h"
+
+#include "Shapes/MGFXMaterialShapeVisual.h"
+
+
+UMGFXMaterialShape_Cross::UMGFXMaterialShape_Cross()
+{
+#if WITH_EDITORONLY_DATA
+	MaterialFunction = TSoftObjectPtr<UMaterialFunctionInterface>(FString("/MGFX/MaterialFunctions/MF_MGFX_Shape_Cross.MF_MGFX_Shape_Cross"));
+#endif
+
+	Visuals.Add(CreateDefaultSubobject<UMGFXMaterialShapeStroke>(TEXT("DefaultStroke")));
+}
+
+#if WITH_EDITORONLY_DATA
+TArray<FMGFXMaterialShapeInput> UMGFXMaterialShape_Cross::GetInputs() const
+{
+	TArray<FMGFXMaterialShapeInput> Result;
+
+	Result.Emplace(FMGFXMaterialShapeInput::Float(TEXT("Size"), Size));
+
+	return Result;
+}
+#endif
---
+++ Plugins/MGFX/Source/MGFX/Public/Shapes/MGFXMaterialShape_Cross.h
@@ -0,0 +1,30 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MGFXMaterialShape.h"
+#include "MGFXMaterialShape_Cross.generated.h"
+
+
+/**
+ * A cross shape.
+ */
+UCLASS(DisplayName = "Cross")
+class MGFX_API UMGFXMaterialShape_Cross : public UMGFXMaterialShape
+{
+	GENERATED_BODY()
+
+public:
+	UMGFXMaterialShape_Cross();
+
+	/** The size of the cross. */
+	UPROPERTY(EditAnywhere, Category = "Cross")
+	float Size = 100.f;
+
+	virtual FString GetShapeName() const override { return TEXT("Cross"); }
+
+#if WITH_EDITORONLY_DATA
+	virtual TArray<FMGFXMaterialShapeInput> GetInputs() const override;
+#endif
+};
