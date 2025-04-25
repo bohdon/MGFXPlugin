daeecdaa0c7203a3487bdc212c2bfd1191b20d9f
Bohdon Sayre <bohdon@gmail.com>
2025-04-25 01:50:05 -0400

    add arc shape

---
+++ Plugins/MGFX/Source/MGFX/Private/Shapes/MGFXMaterialShape_Arc.cpp
@@ -0,0 +1,34 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+
+#include "Shapes/MGFXMaterialShape_Arc.h"
+
+#include "MGFXMaterialFunctionHelpers.h"
+#include "Shapes/MGFXMaterialShapeVisual.h"
+
+
+UMGFXMaterialShape_Arc::UMGFXMaterialShape_Arc()
+{
+	ShapeName = TEXT("Arc");
+	MaterialFunction = FMGFXMaterialFunctions::GetShape(ShapeName);
+	DefaultVisualsClass = UMGFXMaterialShapeFill::StaticClass();
+}
+
+#if WITH_EDITOR
+FBox2D UMGFXMaterialShape_Arc::GetBounds() const
+{
+	const FVector2D HalfSize(Size * 0.5f);
+	return FBox2D(-HalfSize, HalfSize);
+}
+
+TArray<FMGFXMaterialShapeInput> UMGFXMaterialShape_Arc::GetInputs() const
+{
+	TArray<FMGFXMaterialShapeInput> Result;
+
+	Result.Emplace(FMGFXMaterialShapeInput::Float(TEXT("Size"), Size));
+	Result.Emplace(FMGFXMaterialShapeInput::Float(TEXT("Width"), Width));
+	Result.Emplace(FMGFXMaterialShapeInput::Float(TEXT("Sweep"), Sweep));
+
+	return Result;
+}
+#endif
---
+++ Plugins/MGFX/Source/MGFX/Public/Shapes/MGFXMaterialShape_Arc.h
@@ -0,0 +1,37 @@
+﻿// Copyright Bohdon Sayre, All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MGFXMaterialShape.h"
+#include "MGFXMaterialShape_Arc.generated.h"
+
+/**
+ * An arc
+ */
+UCLASS(DisplayName = "Arc")
+class MGFX_API UMGFXMaterialShape_Arc : public UMGFXMaterialShape
+{
+	GENERATED_BODY()
+
+public:
+	UMGFXMaterialShape_Arc();
+
+	/** The diameter of the arc. */
+	UPROPERTY(EditAnywhere, Category = "Arc", Meta = (ClampMin = 0))
+	float Size = 100.f;
+
+	/** The width of the arc. */
+	UPROPERTY(EditAnywhere, Category = "Arc", Meta = (ClampMin = 0))
+	float Width = 3.f;
+
+	/** The sweep of the arc. */
+	UPROPERTY(EditAnywhere, Category = "Arc", Meta = (ClampMin = 0, ClampMax = 1))
+	float Sweep = 0.5f;
+
+#if WITH_EDITOR
+	virtual bool HasBounds() const override { return true; }
+	virtual FBox2D GetBounds() const override;
+	virtual TArray<FMGFXMaterialShapeInput> GetInputs() const override;
+#endif
+};
